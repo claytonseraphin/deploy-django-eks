0d38e2f0c13d0281e845ca77a13b9d4570bf3238
Claytonseraphin <claytonseraphin@users.noreply.github.com>
2026-03-20 14:10:32 -0400

    added rds password variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,10 @@ variable "prefix" {
   description = "Prefix to use for resource names"
   default     = "django-k8s"
 }
+
+variable "db_password" {
+  description = "Password for the RDS database instance"
+  type        = string
+  sensitive   = true
+  default     = "samplepassword123"
+}
