b9984973beb5e3e1e157384370eae2bd8a84ce67
Claytonseraphin <claytonseraphin@users.noreply.github.com>
2026-03-20 14:01:15 -0400

    Added vpc configuration

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,9 @@
 variable "region" {
   description = "AWS region to deploy resources to"
-  default = "us-east-1"
+  default     = "us-east-1"
+}
+
+variable "prefix" {
+  description = "Prefix to use for resource names"
+  default     = "django-k8s"
 }
