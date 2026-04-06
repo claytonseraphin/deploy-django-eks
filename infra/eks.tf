6b9c0c1745ccadab89914c0f80ede0f9ad5abbe4
Claytonseraphin <claytonseraphin@users.noreply.github.com>
2026-04-06 10:25:35 -0400

    Added eks and ecr services

--- infra/eks.tf
+++ infra/eks.tf
@@ -13,12 +13,33 @@ module "eks" {
   subnet_ids                   = module.vpc.private_subnets
   endpoint_public_access       = true
   endpoint_public_access_cidrs = ["0.0.0.0/0"] # you can also pass your machine ip address to restrict access to your computer only.
+  endpoint_private_access      = true
+
+  addons = {
+    vpc-cni = {
+      before_compute           = true
+      most_recent              = true
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+    }
+    kube-proxy = {
+      before_compute = true
+      most_recent    = true
+    }
+    coredns = {
+      most_recent = true
+    }
+  }
+
+  iam_role_additional_policies = {
+    AllowECRApp   = aws_iam_policy.allow_ecr_app.arn
+    AllowECRProxy = aws_iam_policy.allow_ecr_proxy.arn
+  }
 
   eks_managed_node_groups = {
     general = {
       name           = "k8s-ng-1"
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3
